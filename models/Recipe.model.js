3ae78b036896c8a73681e6b7cb76db2b58e4920f
Maël Odina Eugène <maodinae@gmail.com>
2025-09-03 19:49:45 +0200

    lab express-mongoose done

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,1 +1,44 @@
 // Your code here ...
+const mongoose = require("mongoose");
+
+// schema
+const recipeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  instructions: {
+    type: String,
+    required: true,
+  },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
+  },
+  ingredients: {
+    type: [String], // an array of strings
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg",
+  },
+  duration: {
+    type: Number,
+    min: 0,
+  },
+  isArchived: {
+    type: Boolean,
+    default: false,
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// model
+const Artist = mongoose.model("Artist", artistSchema);
+// argument 1: the internal name of the model. Always singular and Pascal Casing.
+
+module.exports = Artist;
